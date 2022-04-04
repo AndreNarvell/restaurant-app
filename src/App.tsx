efd7c015a262b215771b69a98e5bf93d5c6a66fa
AndreNarvell <andrenarvell@hotmail.com>
2022-04-04 13:06:31 +0200

    Trying fetch - works

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
 
 function App() {
-  return <div className="App"></div>;
+  const fetchRestaurant = async () => {
+    const response = await axios.get(
+      "https://school-restaurant-api.azurewebsites.net/restaurant/624abd70df8a9fb11c3ea8b8"
+    );
+    console.log(response.data);
+  };
+
+  return (
+    <div className="App">
+      <button onClick={fetchRestaurant}>hejehejh</button>
+    </div>
+  );
 }
 
 export default App;
