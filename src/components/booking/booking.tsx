cf48b9651bf5959a521c5c920bad845a96dd395f
Nathalie Persson <nathalie.persson@hotmail.com>
2022-04-05 14:39:31 +0200

    Added calendar and slider

--- src/components/booking/booking.tsx
+++ src/components/booking/booking.tsx
@@ -1,9 +1,42 @@
 import "./booking.scss";
+import { Slider } from '@material-ui/core';
+import Calendar from 'react-calendar';
+import { useState } from "react";
 
 const Booking = () => {
-  return <div className="bookingContainer">
-  <div className="bookTable"><p>book a table</p></div>
-  </div>;
+  const [amount, setAmount] = useState([1]);
+  const [date, setDate] = useState(new Date());
+
+  //https://mui.com/components/slider/
+  const updateAmount = (e: any, amount: any) => {
+    setAmount(amount);
+    console.log(amount);
+  }
+
+  const handleDate = (date: Date) => {
+    setDate(date);
+    console.log(date);
+  }
+
+  return (
+    <div className="bookingContainer">
+      <div className="bookTable"><p>book a table</p></div>
+
+      <div style={{ width: 300, margin: 30 }}>
+        <Slider
+          min={1}
+          max={6}
+          onChange={updateAmount}
+          value={amount}
+        />
+        {amount}
+      </div>
+      <Calendar onChange={handleDate} value={date} />
+      <p>
+        {date.toString()}
+      </p>
+    </div>
+  );
 };
 
 export default Booking;
