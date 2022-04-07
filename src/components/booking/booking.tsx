b7a8bf7aa2f7c5c79c66aa32a73543337928b565
AndreNarvell <andrenarvell@hotmail.com>
2022-04-07 14:37:53 +0200

    Started conditional rendering

--- src/components/booking/booking.tsx
+++ src/components/booking/booking.tsx
@@ -1,13 +1,17 @@
 import "./booking.scss";
 import { Slider } from "@material-ui/core";
 import Calendar from "react-calendar";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import moment from "moment";
 
 const Booking = () => {
   const [amount, setAmount] = useState([1]);
   const [date, setDate] = useState(new Date());
-  // const [date, setDate] = useState(moment().format('MM/DD/YYYY'));
+  const [showDate, setShowDate] = useState(false);
+
+  function handleDateClick() {
+    setShowDate(!showDate);
+  }
 
   const updateAmount = (e: any, amount: any) => {
     setAmount(amount);
@@ -19,38 +23,8 @@ const Booking = () => {
     console.log(date);
   };
 
-  return (
-    <div className="bookingContainer">
-      <div>
-        <div className="bookTable">
-          <h1>book a table</h1>
-        </div>
-      </div>
-
-      {/* Slider */}
-      <div className="sliderDiv">
-        <h2>
-          How many is in your <span className="goldenSpan">party?</span>
-        </h2>
-        <div style={{ width: 300, margin: 30 }}>
-          <Slider min={1} max={6} onChange={updateAmount} value={amount} />
-          {amount}
-        </div>
-      </div>
-
-      {/* Calendar */}
-      <div className="calendarDiv">
-        <h2>
-          When would you like to <span className="goldenSpan">party?</span>
-        </h2>
-        <Calendar
-          onChange={handleDate}
-          value={date}
-          minDate={moment().toDate()}
-        />
-        <p>{date.toISOString().split("T")[0]}</p>
-      </div>
-
+  const timeSection = () => {
+    return (
       <div className="chooseTimeDiv">
         <h2>
           Available <span className="goldenSpan">party times:</span>
@@ -63,7 +37,11 @@ const Booking = () => {
           <p>21.00</p>
         </button>
       </div>
+    );
+  };
 
+  const formBookingSection = () => {
+    return (
       <div className="formDiv">
         <form>
           <input type="text" placeholder="First name.." />
@@ -81,8 +59,51 @@ const Booking = () => {
             <p>Make reservation</p>
           </button>
         </div>
-
       </div>
+    );
+  };
+
+  return (
+    <div className="bookingContainer">
+      <div>
+        <div className="bookTable">
+          <h1>book a table</h1>
+        </div>
+      </div>
+
+      {/* Slider */}
+      <div className="sliderDiv">
+        <h2>
+          How many is in your <span className="goldenSpan">party?</span>
+        </h2>
+        <div style={{ width: 300, margin: 30 }}>
+          <Slider
+            min={1}
+            max={6}
+            onChange={updateAmount}
+            value={amount}
+            disabled={showDate}
+          />
+          {amount}
+        </div>
+      </div>
+
+      <button onClick={handleDateClick} disabled={showDate}>
+        test
+      </button>
+      {showDate && (
+        <div className="calendarDiv">
+          <h2>
+            When would you like to <span className="goldenSpan">party?</span>
+          </h2>
+          <Calendar
+            onChange={handleDate}
+            value={date}
+            minDate={moment().toDate()}
+          />
+          <p>{date.toISOString().split("T")[0]}</p>
+        </div>
+      )}
     </div>
   );
 };
