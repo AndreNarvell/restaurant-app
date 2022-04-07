5e52b936e24561ede6de715f53b0d4a5c2eca62d
AndreNarvell <andrenarvell@hotmail.com>
2022-04-07 10:57:51 +0200

    Booking page additions

--- src/components/booking/booking.tsx
+++ src/components/booking/booking.tsx
@@ -2,10 +2,12 @@ import "./booking.scss";
 import { Slider } from "@material-ui/core";
 import Calendar from "react-calendar";
 import { useState } from "react";
+import moment from "moment";
 
 const Booking = () => {
   const [amount, setAmount] = useState([1]);
   const [date, setDate] = useState(new Date());
+  // const [date, setDate] = useState(moment().format('MM/DD/YYYY'));
 
   //https://mui.com/components/slider/
   const updateAmount = (e: any, amount: any) => {
@@ -13,7 +15,7 @@ const Booking = () => {
     console.log(amount);
   };
 
-  const handleDate = (date: Date) => {
+  const handleDate = (date: any) => {
     setDate(date);
     console.log(date);
   };
@@ -21,18 +23,47 @@ const Booking = () => {
   return (
     <div className="bookingContainer">
       <div>
-        <button className="bookTable">
-          <p>book a table</p>
+        <div className="bookTable">
+          <h1>book a table</h1>
+        </div>
+      </div>
+
+      {/* Slider */}
+      <div className="sliderDiv">
+        <h2>
+          How many is in your <span className="golden-span">party?</span>
+        </h2>
+        <div style={{ width: 300, margin: 30 }}>
+          <Slider min={1} max={6} onChange={updateAmount} value={amount} />
+          {amount}
+        </div>
+      </div>
+
+      {/* Calendar */}
+      <div className="calendarDiv">
+        <h2>
+          When would you like to <span className="golden-span">party?</span>
+        </h2>
+        <Calendar
+          onChange={handleDate}
+          value={date}
+          minDate={moment().toDate()}
+        />
+        <p>{date.toISOString().split("T")[0]}</p>
+      </div>
+
+      <div className="chooseTimeDiv">
+        <h2>
+          Available <span className="golden-span">party times:</span>
+        </h2>
+        <button className="primary-button">
+          <p>18.00</p>
+        </button>
+
+        <button className="primary-button">
+          <p>21.00</p>
         </button>
       </div>
-      {/* Slider */}
-      <div style={{ width: 300, margin: 30 }}>
-        <Slider min={1} max={6} onChange={updateAmount} value={amount} />
-        {amount}
-      </div>
-      {/* Calendar */}
-      <Calendar onChange={handleDate} value={date} />
-      <p>{date.toString()}</p>
     </div>
   );
 };
