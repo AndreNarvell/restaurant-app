873d66c39551e66b99070f7eeda32c953991be15
stevekindstrand <stevekindstrand@gmail.com>
2022-04-08 11:03:46 +0200

    conditional rendering-booking

--- src/components/booking/booking.tsx
+++ src/components/booking/booking.tsx
@@ -1,46 +1,32 @@
 import "./booking.scss";
 import { Slider } from "@material-ui/core";
 import Calendar from "react-calendar";
-import { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 import moment from "moment";
 import "animate.css";
 
 const Booking = () => {
   const [amount, setAmount] = useState([0]);
   const [date, setDate] = useState(new Date());
+  const [time, setTime] = useState("");
   const [showDate, setShowDate] = useState(false);
-
-  function handleDateClick() {
-    setShowDate(true);
-  }
+  const [showTime, setShowTime] = useState(false);
 
   const updateAmount = (e: any, amount: any) => {
-    if()
-    setAmount(amount);
-    setShowDate(true);
-    console.log(amount);
+    if (amount > 0) {
+      setAmount(amount);
+      setShowDate(true);
+    }
   };
 
   const handleDate = (date: any) => {
     setDate(date);
-    console.log(date);
+    setShowTime(true);
   };
 
-  const timeSection = () => {
-    return (
-      <div className="chooseTimeDiv">
-        <h2>
-          Available <span className="goldenSpan">party times:</span>
-        </h2>
-        <button className="primaryBtn">
-          <p>18.00</p>
-        </button>
-
-        <button className="primaryBtn">
-          <p>21.00</p>
-        </button>
-      </div>
-    );
+  const handleTime = (e: any) => {
+    const value = e.currentTarget.value;
+    setTime(value);
   };
 
   const formBookingSection = () => {
@@ -96,6 +82,23 @@ const Booking = () => {
             minDate={moment().toDate()}
           />
           <p>{date.toISOString().split("T")[0]}</p>
+        </div>
+      )}
+
+      {showTime && (
+        <div className="chooseTimeDiv">
+          <h2>
+            Available <span className="goldenSpan">party times:</span>
+          </h2>
+          <button className="primaryBtn" onClick={handleTime} value="18:00">
+            <p>18.00</p>
+          </button>
+
+          <button className="primaryBtn" onClick={handleTime} value="21:00">
+            <p>21.00</p>
+          </button>
+
+          <p>{time}</p>        
         </div>
       )}
     </div>
