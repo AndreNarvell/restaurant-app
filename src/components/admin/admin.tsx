0bee22841e858e44ca687adc1cc94ce394e09b7d
AndreNarvell <andrenarvell@hotmail.com>
2022-04-12 09:33:48 +0200

    dont know, nothing works WIP

--- src/components/admin/admin.tsx
+++ src/components/admin/admin.tsx
@@ -1,12 +1,13 @@
 import "./admin.scss";
 import axios from "axios";
 import { useEffect, useState } from "react";
-// import { ICustomer } from "../../models/ICustomer";
+import { ICustomer } from "../../models/ICustomer";
 import { IGetBooking } from "../../models/IGetBooking";
 
 const Admin = () => {
   const [getBooking, setGetBooking] = useState<IGetBooking[]>([]);
-  // const [getCustomer, setGetCustomer] = useState<ICustomer[]>([]);
+  const [getCustomer, setGetCustomer] = useState<ICustomer[]>([]);
+  // const [customerName, setCustomerName] = useState("");
 
   //Lägg till i en service komponent? hjälp
   const fetchBooking = async () => {
@@ -14,32 +15,71 @@ const Admin = () => {
       "https://school-restaurant-api.azurewebsites.net/booking/restaurant/624abd70df8a9fb11c3ea8b8"
     );
 
-    setGetBooking(response.data)
-    console.log(response.data);
+    setGetBooking(response.data);
+    // console.log(response.data[1]._id);
+    let customerIdFromFetch = response.data[1]._id;
+
+    const responseTwo = await axios.get<ICustomer[]>(
+      "https://school-restaurant-api.azurewebsites.net/customer/" +
+        customerIdFromFetch
+    );
+
+    console.log(responseTwo.data[1].name);
   };
+
+  // const fetchCustomer = async (id: any) => {
+  // const response = await axios.get<ICustomer[]>(
+  //   "https://school-restaurant-api.azurewebsites.net/customer/" + id
+  // );
+
+  //   setGetCustomer(response.data);
+  //   console.log(response.data);
+  //   console.log("heeej", id);
+
+  //   // setCustomerName(response.data[0].name);
+  // };
 
   //   "https://school-restaurant-api.azurewebsites.net/booking/delete/625300b3dc2b88e11b1f8204"
   //"https://school-restaurant-api.azurewebsites.net/customer/"
 
   useEffect(() => {
     fetchBooking();
+    // fetchCustomer()
   }, []);
 
+  let customer = getCustomer.map((customer: ICustomer) => {
+    return (
+      <div>
+        <p> {customer.name} </p>
+      </div>
+    );
+  });
+
   //Unique key prop? hjälppp!!
-  let bookings = getBooking.map((booking: IGetBooking, i: number) => {
+  let bookings = getBooking.map((booking: IGetBooking) => {
     return (
       <div className="bookingCard" key={booking._id}>
-        <h2>Table: {i + 1}</h2>
-        <p><strong>Date & Time: </strong>{booking.date}, {booking.time}</p>
-        <p><strong>Guest: </strong>{booking.numberOfGuests}</p>
-        <p><strong>Booking Id: </strong>{booking._id}</p>
+        <p>
+          <strong>Date & Time: </strong>
+          {booking.date}, {booking.time}
+        </p>
+        <p>
+          <strong>Guest: </strong>
+          {booking.numberOfGuests}
+        </p>
+        <p>
+          <strong>Booking Id: </strong>
+          {booking._id}
+        </p>
       </div>
     );
   });
 
   return (
     <div className="cardContainer">
-        {bookings}
+      {/* <button onClick={fetchCustomer}>test</button> */}
+      {customer}
+      {bookings}
     </div>
   );
 };
